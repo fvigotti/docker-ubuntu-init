d9477c09ea8d192bc0a44e59110224ff6686bae7
francesco <histats@gmail.com>
2015-01-16 10:55:00 +0000

    capture signal and sent to group of subprocess

--- config/starter.sh
+++ config/starter.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+
+# capture signals
+trap : SIGTERM SIGINT
+
+echo $$
+
+
 for init in /config/init*; do
     echo 'executing: '$init
     chmod +x $init ;
-	bash "$init"
+	bash "$init" &
+    COMMAND_PID=$!
+    wait $COMMAND_PID
+    if [[ $? -gt 128 ]]
+    then
+        ps auxf
+        echo 'process group killing > '${COMMAND_PID}
+        kill -- -${COMMAND_PID}
+        ps auxf
+        exit 0
+    fi
 done
